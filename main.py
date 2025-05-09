1257798d1b16d5005fdd7f35ae3a78c8de80bcfa
gaoyujin <gaoyujin@shengpay.com>
2025-05-09 11:20:18 +0800

    关键字描述修改

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def change_run(tip):
         print(f"{Fore.LIGHTGREEN_EX}{Style.BRIGHT}输入==》 1 or 图纸目录 or 目录 (送检Excel文件转换){Style.RESET_ALL}")
         print(f"{Fore.LIGHTGREEN_EX}{Style.BRIGHT}输入==》 2 or 清单list or 清单 (PDF等文件信息收集){Style.RESET_ALL}")
         print(f"{Fore.LIGHTGREEN_EX}{Style.BRIGHT}输入==》 3 or CCS特殊字符 or CCS (去除目录中文件的特殊字符){Style.RESET_ALL}")
-        print(f"{Fore.LIGHTGREEN_EX}{Style.BRIGHT}输入==》 4 or A_S or A_ (文件改名){Style.RESET_ALL}")
+        print(f"{Fore.LIGHTGREEN_EX}{Style.BRIGHT}输入==》 4 or ABS命名规则 or ABS (文件改名){Style.RESET_ALL}")
         print(f"{Fore.LIGHTGREEN_EX}{Style.BRIGHT}# 模式切换说明：{Style.RESET_ALL}")
         print(f"{Fore.LIGHTGREEN_EX}{Style.BRIGHT}输入==》'切换'后按回车键{Style.RESET_ALL}")
 
@@ -47,14 +47,16 @@ def change_run(tip):
             run('fileName')
         except KeyboardInterrupt:
             change_run('no')
-    if std == '4' or std == 'A_S' or std.startswith('A_'):
+    if std == '4' or std == 'ABS命名规则' or std.startswith('ABS'):
         try:
             run('letterName')
         except KeyboardInterrupt:
             change_run('no')
-    if std != '1' and std != '2' and std != '3' and std != '4':
+    if (std != '1' and std != '2' and std != '3' and std != '4' and
+            std != '图纸目录' and std != '清单list' and std != 'CCS特殊字符' and std != 'ABS命名规则' and
+            std != '目录' and std != '清单' and std != 'CCS' and std != 'ABS'):
         is_run = True
-        print(f"{Fore.RED}主人，输入的操作编号不存在{Style.RESET_ALL}")
+        print(f"{Fore.RED}主人，输入的操作编号或者关键字不存在{Style.RESET_ALL}")
 
     while is_run:
         change_run('no')
@@ -66,7 +68,7 @@ try:
     print(f"{Fore.LIGHTGREEN_EX}{Style.BRIGHT}输入==》 1 or 图纸目录 or 目录 (送检Excel文件转换){Style.RESET_ALL}")
     print(f"{Fore.LIGHTGREEN_EX}{Style.BRIGHT}输入==》 2 or 清单list or 清单 (PDF等文件信息收集){Style.RESET_ALL}")
     print(f"{Fore.LIGHTGREEN_EX}{Style.BRIGHT}输入==》 3 or CCS特殊字符 or CCS (去除目录中文件的特殊字符){Style.RESET_ALL}")
-    print(f"{Fore.LIGHTGREEN_EX}{Style.BRIGHT}输入==》 4 or A_S or A_ (文件改名){Style.RESET_ALL}")
+    print(f"{Fore.LIGHTGREEN_EX}{Style.BRIGHT}输入==》 4 or ABS命名规则 or ABS (文件改名){Style.RESET_ALL}")
     print(f"{Fore.LIGHTGREEN_EX}{Style.BRIGHT}# 模式切换说明：{Style.RESET_ALL}")
     print(f"{Fore.LIGHTGREEN_EX}{Style.BRIGHT}输入==》'切换'后按回车键{Style.RESET_ALL}")
     change_run('no')
